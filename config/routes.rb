26fb0d5c9c9b2ccd5c9d2688935d43dcfea7a59c
cheltor <rchelton@umd.edu>
2022-02-08 19:48:36 -0500

    init + devise

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "students", to: "devise/sessions#new"
+  end
+  
+  devise_for :students
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "pages#home"
+end
